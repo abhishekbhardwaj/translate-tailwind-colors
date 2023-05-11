52f132bb7c1d83567daaaacc57727f1f6d08198a
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2023-05-10 18:45:59 -0700

    feat: ✨ add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import { AppProps } from 'next/app'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 import '@/styles/tailwind.css'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews />
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
